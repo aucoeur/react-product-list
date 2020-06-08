d193865cc826969f3d6a90a07fc9960d1d62221e
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-06-08 15:20:53 -0700

    Completed challenge 7

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Categories from './data'
+
+// Challenge 7 - Import your exported data into the App component. 
+import data, { categoriesUnique, categoryCounts, objInventory } from './data';
 
 function App() {
+    const items = objInventory.map(({name, count}) => {
+      return <li>{name}: {count}</li>
+    }
+  )
+
   return (
-    <Categories />
-  );
+    <ul> {items}</ul>
+  )
 }
 
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import data from './data.json' // imports data.json
 
-function Categories() {
-    const categories = data.map(( { category }, i ) => {
-        
-        return (
-            <li> {category}</li>
-        )
-    })
-
+// Challenge 1 - Get the array of products.
+// Challenge 2 - Get a list of all categories.
+const categories = data.map(( { category }, i ) => {
     return (
-        <ul>
-            {categories}
-        </ul>
+        category
     )
-}
+})
 
-export default Categories;
+// Challenge 3 - Make the categories list a list of unique values.
+const setCategories = new Set(categories) // Set returns unique values only
+const categoriesUnique = Array.from(setCategories) // Make an array from set
+// console.log(categoriesUnique)
+
+// Challenge 4 - Make an Object whose keys are the names of categories and whose values are the number of times that category appears in the data.
+const categoryCounts = data.reduce(( item, {category}) => {
+    if (item[category] !== undefined) {
+        item[category] += 1
+    } else {
+        item[category] = 1
+    }
+    return item
+    }, {} // set init value to {} 
+    )
+    // console.log(categoryCounts)
+
+// Challenge 5 - Use Reduce to make an array of objects that have a name and a count. This array will be similar to Challenge 4 but in a different format. For this list all of the names should be unique and each should only appear once!
+
+// const objInventory = Object.entries(categoryCounts) // can we do like this??
+const objInventory = Object.entries(categoryCounts).reduce((arr, entry) => {
+    const [key, value] = entry
+    arr.push({name: key, count: value})
+    return arr
+}, [])
+console.log(objInventory)
+
+ 
+
+// Challenge 6 - Export all of the data you have collected.
+export default data;
+export { categoriesUnique, categoryCounts, objInventory }
