6fa0c91ce5dd1fa314f6cceae21a67d14b6b65a7
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-06-10 22:22:37 -0700

    Updates

---
+++ precache-manifest.d8a008741caf8970dfb768b131f77f48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7a5fa88e57c81dd43294c1e0a0a07c",
+    "url": "/react-product-list/index.html"
+  },
+  {
+    "revision": "9d3c64c4ee18aef01d8e",
+    "url": "/react-product-list/static/css/main.2f63e7cd.chunk.css"
+  },
+  {
+    "revision": "f7d885a14432732a1886",
+    "url": "/react-product-list/static/js/2.1eb0f143.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-product-list/static/js/2.1eb0f143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3c64c4ee18aef01d8e",
+    "url": "/react-product-list/static/js/main.10823c20.chunk.js"
+  },
+  {
+    "revision": "c2eef72beb4bfec90832",
+    "url": "/react-product-list/static/js/runtime-main.8f23e73c.js"
+  }
+]);
